69d22b46e86d18d200c32cc8caa886118e2ec48a
MOHAM <mohamedmadkour47@gamil>
2018-07-06 00:22:46 +0200

    add home activity (not complete )

---
+++ src/main/java/com/github/pires/obd/reader/activity/HomeActivity.java
@@ -0,0 +1,27 @@
+package com.github.pires.obd.reader.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.github.pires.obd.reader.R;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void openSitting(View view) {
+        startActivity(new Intent(this, ConfigActivity.class));
+    }
+    public void openApi(View view) {
+        startActivity(new Intent(this, Search.class));
+    }
+    public void openTripList(View view) {
+        startActivity(new Intent(this, TripListActivity.class));
+    }
+}
